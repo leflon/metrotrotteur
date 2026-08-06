4d51f5499741153a068dbb8532a0d49f73308e18
leflon <paul@leflon.fr>
2026-08-06 23:57:19 +0200

    update socket parameters

--- packages/frontend/src/lib/socket.ts
+++ packages/frontend/src/lib/socket.ts
@@ -4,7 +4,8 @@ export function createSocket() {
   const token = localStorage.getItem('mtst') ?? crypto.randomUUID();
   localStorage.setItem('mtst', token);
 
-  const socket = io(import.meta.env.VITE_PUBLIC_API_URL, {
+  const socket = io(import.meta.env.VITE_PUBLIC_SOCKET_URL, {
+    path: import.meta.env.VITE_PUBLIC_SOCKET_PATH,
     auth: {
       token,
       name: localStorage.getItem('mtpu')
