ade012e90c28f4f27944ea159af55eebfd4ce35d
leflon <paul@leflon.fr>
2026-08-06 23:44:07 +0200

    add footer

--- packages/frontend/src/views/HomeView.vue
+++ packages/frontend/src/views/HomeView.vue
@@ -1,20 +1,24 @@
-<script setup lang='ts'>
-import { User, Users } from '@lucide/vue';
+<script setup lang="ts">
+import { User, Users } from "@lucide/vue";
 </script>
 
 <template>
-<div class='main-menu flex column center'>
-  <h1 class='logo'>Métro Trotteur</h1>
-  <div class='buttons flex column center'>
-    <router-link to='/solo' class='home-btn solo'>
-      <User />
-      Solo
+  <div class="main-menu flex column center">
+    <h1 class="logo">Métro Trotteur</h1>
+    <div class="buttons flex column center">
+      <router-link to="/solo" class="home-btn solo">
+        <User />
+        Solo
       </router-link>
-    <router-link to='/multi' class='home-btn multi'>
-      <Users />
-      Multijoueur
-    </router-link>
-  </div>
+      <router-link to="/multi" class="home-btn multi">
+        <Users />
+        Multijoueur
+      </router-link>
+    </div>
+  <footer>
+    <p>Créé par <a href='https://github.com/leflon' target='_blank'>Paul Leflon</a></p>
+    <p>Illustrations par <a href='https://www.linkedin.com/in/youjinogsol/' target='_blank'>Youjin Jeong</a></p>
+  </footer>
   </div>
 </template>
 
@@ -23,25 +27,29 @@ import { User, Users } from '@lucide/vue';
   height: 100%;
 }
 .logo {
-  font: bold 28pt 'Parisine';
+  font: bold 28pt "Parisine";
   position: relative;
   text-align: center;
   &:after {
     top: 0;
-    content: 'INDEV';
+    content: "INDEV";
     font: bold 11pt monospace;
     background: #fcbb08;
     color: red;
     padding: 2px 4px;
     position: absolute;
-    transform: rotate(22deg) translate(-30px,5px);
+    transform: rotate(22deg) translate(-30px, 5px);
   }
 }
-.solo { --color: #83C491; }
-.multi { --color: #F28E42;}
+.solo {
+  --color: #83c491;
+}
+.multi {
+  --color: #f28e42;
+}
 .home-btn {
   width: 100%;
-  font: bold 18pt 'Parisine';
+  font: bold 18pt "Parisine";
   padding: 12px 40px;
   color: #222;
   background: white;
@@ -52,22 +60,42 @@ import { User, Users } from '@lucide/vue';
   align-items: center;
   gap: 10px;
   text-decoration: none;
-  transition: .3s ease;
+  transition: 0.3s ease;
 
   &:hover {
     transform: scale(1.05);
     color: var(--color);
     border-color: var(--color);
-    background: color-mix(in srgb, var(--color) 10%, white)
+    background: color-mix(in srgb, var(--color) 10%, white);
   }
   &:active {
-    transform: scale(.95);
+    transform: scale(0.95);
   }
 
   &.disabled {
     pointer-events: none;
     cursor: not-allowed;
-    opacity: .5;
+    opacity: 0.5;
+  }
+}
+
+footer {
+  position: absolute;
+  bottom: 10px;
+  color: #000;
+  text-align: center;
+  opacity: .5;
+  font-size: 9pt;
+
+  & p {
+    margin: 3px 0;
+  }
+
+  & a {
+    color: inherit;
+    &:hover {
+      text-decoration: none;
+    }
   }
 }
 </style>
